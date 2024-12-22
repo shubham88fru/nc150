5da1b0b2bd4ddf6d238c8d57cc951781c07be3cd
shubham88fru <shubham88fru@yahoo.com>
2024-12-22 09:31:50 -0700

    same binary tree

---
+++ easy/same_binary_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+from typing import Optional
+
+
+# @link - https://neetcode.io/problems/same-binary-tree
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.dfs(p, q)
+
+    def dfs(self, p, q):
+        if p is None and q is None:
+            return True
+
+        if p is None or q is None:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        left = self.dfs(p.left, q.left)
+        right = self.dfs(p.right, q.right)
+
+        return left and right
