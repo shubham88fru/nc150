f7c72a68a3f2d4b3308fedc9e005fb974fc2a87e
shubham88fru <shubham88fru@yahoo.com>
2024-12-26 06:18:40 +0000

    single number

---
+++ easy/single_number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+# @link - https://neetcode.io/problems/single-number
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        return xor
