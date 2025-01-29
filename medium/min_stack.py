e1998c827e48400c89684ef1cf66c7a895c6159f
shubham88fru <shubham88fru@yahoo.com>
2025-01-28 20:18:31 -0700

    min stack

---
+++ medium/min_stack.py
@@ -0,0 +1,28 @@
+
+# @link - https://neetcode.io/problems/minimum-stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.mini = 2 ** 63
+
+    def push(self, val: int) -> None:
+        if val < self.mini:
+            self.mini = val
+
+        self.stack.append([val, self.mini])
+
+    def pop(self) -> None:
+        pair = self.stack.pop()
+
+        if len(self.stack) == 0:
+            self.mini = 2 ** 63
+        else:
+            self.mini = self.stack[-1][1]
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.mini
+
