c1aa31ca2546bcf730ddb8726ffb537e21f148c3
shubham88fru <shubham88fru@yahoo.com>
2025-02-08 20:08:23 -0700

    car fleet

--- medium/car_fleet.py
+++ medium/car_fleet.py
@@ -1,9 +1,14 @@
 from typing import List
 
 # @link - https://neetcode.io/problems/car-fleet
+# @check - https://www.youtube.com/watch?v=Pr6T-3yB9RM&ab_channel=NeetCode
 class Solution:
+
+    # Had a hunch but couldn't solve on my own.
+    # Solution based on explanation by nc.
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         sorted_pairs = sorted(zip(position, speed), key=lambda pair: pair[0])
+
         stack = []
         for position, speed in sorted_pairs[::-1]:
             if len(stack) == 0:
