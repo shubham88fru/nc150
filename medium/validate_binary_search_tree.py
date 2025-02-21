30a4e3b9e3989e899334bd6722e76e5009050f70
shubham88fru <shubham88fru@yahoo.com>
2025-02-21 11:37:28 -0700

    validate binary search tree

---
+++ medium/validate_binary_search_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+from typing import Optional
+
+# @link - https://neetcode.io/problems/valid-binary-search-tree
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.validate(root, -5000, 5000)
+
+    def validate(self, root, mn, mx):
+        if root is None:
+            return True
+
+        if root.val <= mn:
+            return False
+
+        if root.val >= mx:
+            return False
+
+        left = self.validate(root.left, mn, root.val)
+        right = self.validate(root.right, root.val, mx)
+
+        return left and right
