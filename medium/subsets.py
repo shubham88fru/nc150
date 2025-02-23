aa84a0c72dd7bb7d7df7b0ac1444de458464749e
shubham88fru <shubham88fru@yahoo.com>
2025-02-23 11:48:41 -0700

    subsets

--- medium/subsets.py
+++ medium/subsets.py
@@ -3,9 +3,29 @@ from typing import List
 # @link - https://neetcode.io/problems/subsets
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        return self.solve(nums)
+        # return self.solve1(nums)
+        return self.solve2(nums)
 
-    def solve(self, nums):
+    # Approach 2: makes subsets II problem straightforward.
+    def solve2(self, nums):
+        ans = []
+        self.bactrack2(nums, 0, ans, [])
+        return ans
+
+    def bactrack2(self, nums, idx, ans, curr):
+        ans.append(list(curr))
+
+        for i in range(idx, len(nums)):
+            curr.append(nums[i])
+            self.bactrack2(nums, i + 1, ans, curr)
+            curr.pop()
+
+    # Approach 1: This is the first approach (based on 01 Knapsack pattern)
+    # that comes to my mind whenever I encounter this problem.
+    # However, this approach would lead a complicated (and nasty)
+    # code for subsets II problem. So, as a rule of thumb always
+    # solve it using the approach 2.
+    def solve1(self, nums):
         ans = []
         self.backtrack(nums, 0, ans, [])
         return ans
