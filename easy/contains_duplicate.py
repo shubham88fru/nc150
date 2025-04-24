98c4f3b922a13ba1e976d0d20d9314aa1213a0a6
shubham88fru <shubham88fru@yahoo.com>
2025-04-24 11:09:39 -0700

    tdlr; contains duplicate

--- easy/contains_duplicate.py
+++ easy/contains_duplicate.py
@@ -1,6 +1,26 @@
 from typing import List
 
 # @link - https://neetcode.io/problems/duplicate-integer
+""""""""""""""""""""""""""""
+----------------
+OPTIMAL: Hashmap
+----------------
+TC: O(n)
+SC: O(n)
+
+------------------------------------
+BETTER: Sort and compare neighbours.
+------------------------------------
+TC: O(n + nlog(n))
+SC: O(1)
+
+----------------------------------------------
+BRUTE: For each check if exists anywhere else.
+----------------------------------------------
+TC: O(n^2)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         dic = {};
