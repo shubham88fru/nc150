01fe85b00be00a6f1c819d988a0f69a66cb81b91
shubham88fru <shubham88fru@yahoo.com>
2025-04-27 10:00:13 -0700

    product of array except self.

--- medium/product_of_array_except_self.py
+++ medium/product_of_array_except_self.py
@@ -3,6 +3,29 @@ from typing import List
 # @link - https://neetcode.io/problems/products-of-array-discluding-self
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        return self.revise(nums)
+
+    # One pass.
+    def revise(self, nums):
+        n = len(nums)
+        pp = [0] * n
+        sp = [0] * n
+
+        pp[0] = 1
+        sp[n - 1] = 1
+
+        for i in range(1, n):
+            pp[i] = pp[i - 1] * nums[i - 1];
+            sp[n - i - 1] = sp[n - i] * nums[n - i]
+
+        ans = []
+        for i in range(0, n):
+            ans.append(pp[i] * sp[i])
+
+        return ans
+
+    # Two pass.
+    def solve(self, nums):
         pp = [1]
         for i, el in enumerate(nums):
             pp.append(el * pp[len(pp) - 1])
