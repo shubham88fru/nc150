636c6275869f6c63f67e0136420d4351e54fc3d4
shubham88fru <shubham88fru@yahoo.com>
2025-04-28 17:50:58 -0700

    tldr; longest consecutive sequence

--- medium/longest_consecutive_sequence.py
+++ medium/longest_consecutive_sequence.py
@@ -1,8 +1,42 @@
 from typing import List
 
+""""""""""""""""""""""""""""
+----------------
+OPTIMAL: Hashset
+----------------
+TC: O(2n + n)
+SC: O(n)
+
+---------------
+BETTER: Sorting
+---------------
+TC: O(nlog(n) + n)
+SC: O(1)
+
+--------------------------------------------------------------
+BRUTE: Pick each number if keep checking if next can be found.
+--------------------------------------------------------------
+TC: O(n^2)
+SC: O(1)
+
+"""""""""""""""""""""""""""
+
+"""
+Note that  in this problem, we are not supposed
+find the longest consecutive subsequence, instead
+the longest consecutive sequence that can be formed.
+The subtle difference is that in the the next larger
+number is not necessarily needed to be after the previous one.
+The order in which elements appear in the array
+doesn't matter for this problem.
+"""
 # @link - https://neetcode.io/problems/longest-consecutive-sequence
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        return self.solve(nums)
+        # return self.approach2(nums)
+
+    def solve(self, nums):
         st = set()
         for num in nums:
             st.add(num)
@@ -21,3 +55,28 @@ class Solution:
             longest = max(longest, lngst)
 
         return longest
+
+    def approach2(self, nums):
+        n = len(nums)
+        if n == 0: return 0
+
+        nums.sort()
+
+        longest = 1
+        curr = 1
+        i = 1
+
+        while i < n:
+            if nums[i] == nums[i - 1]:
+                i += 1
+                continue
+
+            if nums[i] == nums[i - 1] + 1:
+                curr += 1
+            else:
+                longest = max(longest, curr)
+                curr = 1
+
+            i += 1
+
+        return max(longest, curr)
