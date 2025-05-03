ccd98d7b356e9e6590758935999947b700e86754
shubham88fru <shubham88fru@yahoo.com>
2025-05-02 20:20:39 -0700

    best time to buy and sell stock

--- easy/best_time_to_buy_and_sell_stock.py
+++ easy/best_time_to_buy_and_sell_stock.py
@@ -1,8 +1,32 @@
 from typing import List
 
+""""""""""""""""""""""""""""
+------------------------------------
+OPTIMAL: sliding window/two pointers
+------------------------------------
+TC: O(n)
+SC: O(1)
+
+-------------------
+BETTER: Top down dp
+-------------------
+TC: O(n)
+SC: O(n)
+
+---------------------------------------------------------------
+BRUTE: For each day, find the next higher day we can sell stock
+---------------------------------------------------------------
+TC: O(n^2)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/buy-and-sell-crypto
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # return self.solve(prices)
+        return self.revise(prices)
+
+    def solve(self, prices):
         i = 0
         j = 0
         n = len(prices)
@@ -16,3 +40,15 @@ class Solution:
                 i = j
 
         return mx
+
+    def revise(self, prices):
+        n = len(prices)
+        mini = prices[0]
+        maxProfit = 0
+        for i in range(1, n):
+            if prices[i] < mini:
+                mini = prices[i]
+            else:
+                maxProfit = max(maxProfit, prices[i] - mini)
+
+        return maxProfit
