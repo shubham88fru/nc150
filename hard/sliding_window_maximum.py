6f998f05c3d324277547e77e70e95f2848ee64a9
shubham88fru <shubham88fru@yahoo.com>
2025-05-12 18:50:45 -0700

    tl;dr - sliding window maximum

--- hard/sliding_window_maximum.py
+++ hard/sliding_window_maximum.py
@@ -2,12 +2,32 @@ from typing import List
 
 from sortedcontainers import SortedDict
 
+""""""""""""""""""""""""""""
+------------------------------------
+OPTIMAL: Using deque (see java soln)
+------------------------------------
+TC: O(n)
+SC: O(k)
+
+------------------------------------
+BETTER: Sliding window using treemap
+------------------------------------
+TC: O(nlog(k))
+SC: O(k)
+
+----------------------------------------------------------------
+BRUTE: Keep generating k length windows and finding max in each.
+----------------------------------------------------------------
+TC: O(w*k) where w is the num of windows (n-k+1)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/sliding-window-maximum
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         return self.solve(nums, k)
 
-    # This is sliding window approach.
+    # This is sliding window and treemap approach.
     # Albeit not optimal. Optimal approach
     # uses deque but will need to watch some
     # online soln for that.
