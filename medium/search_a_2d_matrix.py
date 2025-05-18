bce53f9d98b5bba481fe92e1bfde4c24a7915d33
shubham88fru <shubham88fru@yahoo.com>
2025-05-18 11:51:06 -0700

    search a 2d matrix

--- medium/search_a_2d_matrix.py
+++ medium/search_a_2d_matrix.py
@@ -1,8 +1,38 @@
 from typing import List
 
+""""""""""""""""""""""""""""
+---------------------------------------
+OPTIMAL: Binary search on entire matrix
+---------------------------------------
+TC: O(log(m*n))
+SC: O(1)
+
+--------------------------------
+BETTER: Binary search row by row
+--------------------------------
+TC: O(m*log(n))
+SC: O(1)
+
+--------------------------------------------------------
+ADDITIONAL: Start from top right and move to bottom left
+--------------------------------------------------------
+TC: O(m + n)
+SC: O(1)
+
+--------------------
+BRUTE: linear search
+--------------------
+TC: O(m*n)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/search-2d-matrix
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return self.revise(matrix, target)
+        # return self.revise2(matrix, target)
+
+    def revise(self, matrix, target):
         m = len(matrix)
         n = len(matrix[0])
 
@@ -11,17 +41,32 @@ class Solution:
 
         while l <= r:
             mid = l + (r - l) // 2
-            j_ = mid % n
-            i_ = mid // n
-
-            curr = matrix[i_][j_]
-            if curr == target:
+            i = mid // n
+            j = mid % n
+            if matrix[i][j] == target:
                 return True
 
-            if curr > target:
-                r = mid - 1
-            else:
+            if matrix[i][j] < target:
                 l = mid + 1
+            else:
+                r = mid - 1
+
+        return False
+
+    def revise2(self, matrix, target):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        i = 0
+        j = n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+
+            if matrix[i][j] < target:  # need to increase - move down
+                i += 1
+            else:  # need to decrease - move left
+                j -= 1
 
         return False
 
