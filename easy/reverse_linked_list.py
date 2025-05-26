dfda5a3ca720e5cb81d3e95ff901afcfaba24ab0
shubham88fru <shubham88fru@yahoo.com>
2025-05-26 12:14:34 -0700

    tl;dr - reverse linked list

--- easy/reverse_linked_list.py
+++ easy/reverse_linked_list.py
@@ -1,5 +1,25 @@
 from typing import Optional
 
+""""""""""""""""""""""""""""
+-----------------------------
+OPTIMAL: 3 pointers Iterative
+-----------------------------
+TC: O(n)
+SC: O(1)
+
+--------------------------
+BETTER: Recursive solution
+--------------------------
+TC: O(n)
+SC: O(n)
+
+--------------------------------------------
+BRUTE: Use stack. Push to stack and then pop
+--------------------------------------------
+TC: O(2N)
+SC: O(N)
+
+"""""""""""""""""""""""""""
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -9,14 +29,29 @@ class ListNode:
 #@link - https://neetcode.io/problems/reverse-a-linked-list
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.iterative(head)
+        # return self.recursive(head)
+
+    # Based on strvr's explanation
+    def recursive(self, head):
+        if head is None or head.next is None:
+            return head
+
+        new_head = self.recursive(head.next)
+        front = head.next
+        front.next = head
+        head.next = None
+        return new_head
+
+    def iterative(self, head):
         prev = None
-        curr = None
+        curr = head
         nxt = head
 
-        while nxt != None:
-            curr = nxt
-            nxt = nxt.next
+        while curr != None:
+            nxt = curr.next
             curr.next = prev
             prev = curr
+            curr = nxt
 
         return prev
