ca117d4f6d60d5034e2d8506c49c86a25d003705
shubham88fru <shubham88fru@yahoo.com>
2025-08-16 15:11:04 -0500

    tl;dr - reverse nodes in k groups

--- hard/reverse_nodes_in_k_groups.py
+++ hard/reverse_nodes_in_k_groups.py
@@ -1,5 +1,27 @@
 from typing import Optional
 
+""""""""""""""""""""""""""""
+-----------------------
+OPTIMAL: Using pointers
+-----------------------
+TC: O(n)
+SC: O(1)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+-------------------------------
+BRUTE: store in lst and reverse
+-------------------------------
+TC: O(n)
+SC: O(n)
+
+"""""""""""""""""""""""""""
+
+# @link - https://leetcode.com/problems/reverse-nodes-in-k-group/
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -8,7 +30,40 @@ class ListNode:
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        return self.suboptimal(head, k)
+        # return self.suboptimal(head, k)
+        return self.optimal(head, k)
+
+    # Based on nc's explanation.
+    def optimal(self, head, k):
+        dummy = ListNode(-1, head)
+        group_prev = dummy
+
+        while True:
+            kth = self.get_kth(group_prev, k)
+
+            if not kth:
+                break
+
+            group_next = kth.next
+            prev, curr = group_next, group_prev.next
+            while curr != group_next:
+                nxt = curr.next
+                curr.next = prev
+                prev = curr
+                curr = nxt
+
+            tmp = group_prev.next
+            group_prev.next = kth
+            group_prev = tmp
+
+        return dummy.next
+
+    def get_kth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+
+        return curr
 
     # 2) This ain't the optimal
     # approach coz it uses extra
