d824205096829bde1ea7dcd951371162fec22345
shubham88fru <shubham88fru@yahoo.com>
2025-08-17 11:13:58 -0500

    invert binary tree

--- easy/invert_binary_tree.py
+++ easy/invert_binary_tree.py
@@ -1,7 +1,26 @@
 # Definition for a binary tree node.
 from typing import Optional
 
+""""""""""""""""""""""""""""
+------------------------------------
+OPTIMAL: DFS (Preorder or Postorder)
+------------------------------------
+TC: O(n)
+SC: O(1)
 
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+----------------------------------------------
+BRUTE:
+----------------------------------------------
+TC:
+SC:
+
+"""""""""""""""""""""""""""
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -11,18 +30,16 @@ class TreeNode:
 # @link - https://neetcode.io/problems/invert-a-binary-tree
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        self.dfs(root)
+        return self.revise(root)
+
+    def revise(self, root):
+        if not root:
+            return None
+
+        left = self.revise(root.left)
+        right = self.revise(root.right)
+
+        root.left = right
+        root.right = left
+
         return root
-
-    def dfs(self, root):
-        if root is None:
-            return
-
-        self.dfs(root.left)
-        self.dfs(root.right)
-
-        temp = root.right
-        root.right = root.left
-        root.left = temp
-
-        return
