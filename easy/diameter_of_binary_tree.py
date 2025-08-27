901b03ebbb8f69c4a8a95f06eec126428c701239
shubham88fru <shubham88fru@yahoo.com>
2025-08-27 18:58:53 -0500

    tl;dr - diameter of binary tree

--- easy/diameter_of_binary_tree.py
+++ easy/diameter_of_binary_tree.py
@@ -1,3 +1,23 @@
+""""""""""""""""""""""""""""
+------------
+OPTIMAL: DFS
+------------
+TC: O(n)
+SC: O(1), only recursive stack space.
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+----------------------------------------------
+BRUTE:
+----------------------------------------------
+TC:
+SC:
+
+"""""""""""""""""""""""""""
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,13 +30,16 @@ from typing import Optional
 # @link - https://neetcode.io/problems/binary-tree-diameter
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        return self.dfs(root)[1] - 1
+        return self.revise(root)
 
-    def dfs(self, root):
-        if root is None:
-            return 0, 0
+    def revise(self, root):
+        return self.diam(root)[1]
 
-        left = self.dfs(root.left)
-        right = self.dfs(root.right)
+    def diam(self, root):
+        if not root:
+            return [0, 0]
 
-        return 1 + max(left[0], right[0]), max(left[1], right[1], 1 + left[0] + right[0])
+        left = self.diam(root.left)
+        right = self.diam(root.right)
+
+        return [1 + max(left[0], right[0]), max(max(left[1], right[1]), left[0] + right[0])]
