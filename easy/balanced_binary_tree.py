fe81421fd6e3c99d70a3c84be409f3a46d41e55e
shubham88fru <shubham88fru@yahoo.com>
2025-09-03 20:01:52 -0500

    tl;dr - balanced binary tree

--- easy/balanced_binary_tree.py
+++ easy/balanced_binary_tree.py
@@ -1,4 +1,24 @@
 # Definition for a binary tree node.
+""""""""""""""""""""""""""""
+------------
+OPTIMAL: DFS
+------------
+TC: O(n)
+SC: O(1)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+---------------------------------------------------------
+BRUTE: At each node run dfs for left and right (check nc)
+---------------------------------------------------------
+TC: O(n^2)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -20,3 +40,16 @@ class Solution:
         right = self.dfs(root.right)
 
         return 1 + max(left[0], right[0]), ((abs(left[0] - right[0]) <= 1) and left[1] and right[1])
+
+    def revise(self, root):
+        if not root:
+            return 0, True
+
+        left = self.revise(root.left)
+        right = self.revise(root.right)
+
+        diff = abs(left[0] - right[0])
+        if diff <= 1 and left[1] and right[1]:
+            return 1 + max(left[0], right[0]), True
+
+        return -1, False
