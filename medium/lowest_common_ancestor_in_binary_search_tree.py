ead820cb198026dff627b8058f2d9d9d1ddb2841
shubham88fru <shubham88fru@yahoo.com>
2025-09-14 21:29:32 -0500

    lowest common ancestor in binary search tree

--- medium/lowest_common_ancestor_in_binary_search_tree.py
+++ medium/lowest_common_ancestor_in_binary_search_tree.py
@@ -1,3 +1,23 @@
+""""""""""""""""""""""""""""
+----------------------------------
+OPTIMAL: Smart DFS considering BST
+----------------------------------
+TC: O(log(n))
+SC: O(1)
+
+------------------------------------
+BETTER: 
+------------------------------------
+TC:
+SC:
+
+----------------
+BRUTE: Plain DFS
+----------------
+TC: O(n)
+SC: O(1)
+
+"""""""""""""""""""""""""""
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -8,30 +28,48 @@ class TreeNode:
 # @link - https://neetcode.io/problems/lowest-common-ancestor-in-binary-search-tree
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        return self.sol2(root, p, q)
+        # return self.revise(root, p, q)
+        return self.revise2(root, p, q)
 
-    # Simple DFS, doesn't use the fact that the
-    # given tree is a BST.
-    # In an interview its almost
-    # certain that there will be a
-    # followup.
-    def sol2(self, root, p, q):
-        return self.dfs(root, p, q)
+    # ideal soln.
+    def revise2(self, root, p, q):
+        ans = root
+        curr = root
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
+
+            ans = curr
+
+        return ans
+
+    # okayish soln.
+    def revise(self, root, p, q):
+        return self.dfs(root, p, q)[0]
 
     def dfs(self, root, p, q):
-        if root is None:
-            return None
+        if not root:
+            return None, False
 
         if root.val == p.val or root.val == q.val:
-            return root
+            return root, True
 
-        left = self.dfs(root.left, p, q)
-        right = self.dfs(root.right, p, q)
+        l = None, False
+        if p.val <= root.val or q.val <= root.val:
+            l = self.dfs(root.left, p, q)
 
-        if left is not None and right is not None:
-            return root
+        r = None, False
+        if p.val >= root.val or q.val >= root.val:
+            r = self.dfs(root.right, p, q)
 
-        if left is not None:
-            return left
+        if l[1] and r[1]:
+            return root, True
 
-        return right
+        if l[1]:
+            return l
+
+        return r
