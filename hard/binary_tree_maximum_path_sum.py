a5e6bca2be537041032de10215a73712f9c5d168
shubham88fru <shubham88fru@yahoo.com>
2025-11-17 19:02:16 -0600

    binary tree maximum path sum

--- hard/binary_tree_maximum_path_sum.py
+++ hard/binary_tree_maximum_path_sum.py
@@ -1,3 +1,23 @@
+""""""""""""""""""""""""""""
+------------
+OPTIMAL: DFS
+------------
+TC: O(n)
+SC: O(1)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+----------------------------------------------
+BRUTE:
+----------------------------------------------
+TC:
+SC:
+
+"""""""""""""""""""""""""""
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,7 +30,24 @@ from typing import Optional
 # @link - https://neetcode.io/problems/binary-tree-maximum-path-sum
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        return self.solve(root)
+        # return self.solve(root)
+        return self.revise(root)
+    def revise(self, root):
+        return self.dfs(root)[1]
+
+    # My soln that doesn't use
+    # a global max variable.
+    # Might be a bit difficult
+    # to get right in the first go.
+    def dfs(self, root):
+        if not root:
+            return -99999999, -99999999
+
+        l = self.dfs(root.left)
+        r = self.dfs(root.right)
+
+        mx = max(l[0] + root.val, r[0] + root.val, root.val)
+        return mx, max(mx, l[1], r[1], l[0] + r[0] + root.val)
 
     def solve(self, root):
         maxi = [-99999999999]
