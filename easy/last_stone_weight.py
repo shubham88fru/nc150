d07dc78bf8f88c685d7e5b6a9e72d77d1d4b6bac
shubham88fru <shubham88fru@yahoo.com>
2025-11-25 14:25:36 -0600

    tl:dr - last stone weight

--- easy/last_stone_weight.py
+++ easy/last_stone_weight.py
@@ -1,21 +1,45 @@
 import heapq
 from typing import List
 
+""""""""""""""""""""""""""""
+-----------------
+OPTIMAL: Max heap
+-----------------
+TC: O(n*log(n) + n * (3log(n)) --> O(4nlog(n))
+SC: O(n)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+--------------------------------------------
+BRUTE: Keep sorting and checking largest two
+--------------------------------------------
+TC: O(n*nlog(n))
+SC: O(n)
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/last-stone-weight
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
-        # Trick for max heap in python
-        # unlike java, max heap doesn't come
-        # out of the box in python.
-        mx_heap = [ -stone for stone in stones ]
-        heapq.heapify(mx_heap)
-        while len(mx_heap) != 0:
-            if len(mx_heap) > 1:
-                s = heapq.heappop(mx_heap)
-                ss = heapq.heappop(mx_heap)
-                if s != ss:
-                    heapq.heappush(mx_heap, -abs(s-ss))
-            else:
-                return abs(heapq.heappop(mx_heap))
+        return self.revise(stones)
 
-        return 0
+    def revise(self, stones):
+        n = len(stones)
+        heap = []
+
+        for s in stones:
+            heapq.heappush(heap, -s)
+
+        while len(heap) > 1:
+            l = -heapq.heappop(heap)
+            sl = -heapq.heappop(heap)
+
+            if l == sl:
+                continue
+
+            heapq.heappush(heap, -abs(l - sl))
+
+        return 0 if len(heap) == 0 else -heap[0]
