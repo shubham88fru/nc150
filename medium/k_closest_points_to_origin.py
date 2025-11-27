4c2b592ddbb175efd6e3caea432aa59c15303258
shubham88fru <shubham88fru@yahoo.com>
2025-11-27 17:58:55 -0600

    tl;dr - k closest points to origin

--- medium/k_closest_points_to_origin.py
+++ medium/k_closest_points_to_origin.py
@@ -1,20 +1,55 @@
 import heapq
+import math
 from typing import List
 
+""""""""""""""""""""""""""""
+---------------------------------
+OPTIMAL: Using quick select algo
+--------------------------------
+TC: ??
+SC: ??
+
+--------------
+GOOD: Max heap
+--------------
+TC: O(nlog(k))
+SC: O(k)
+
+------------------------------
+BETTER: Min heap using heapify
+------------------------------
+TC: O(klog(n)) ; note, the TC will be O(n) for heapify op, 
+    as opposed to normal loop to put elements to heap (which will be a O(nlog(n))) op.
+SC: O(n)
+
+--------------
+BRUTE: Sorting
+--------------
+TC: O(nlog(n))
+SC: O(1)
+
+"""""""""""""""""""""""""""
 
 # @link - https://neetcode.io/problems/k-closest-points-to-origin
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        max_heap = []
+        return self.revise(points, k)
 
-        for i in range(0, len(points)):
+    # This is a max heap soln.
+    # Turns out there's an even better soln
+    # using some `quick select` algorithm
+    def revise(self, points, k):
+        n = len(points)
 
-            point = points[i]
-            curr_dist = (point[0] * point[0] + point[1] * point[1])
-            if len(max_heap) < k:
-                heapq.heappush(max_heap, (-curr_dist, (point[0], point[1])))
-            elif curr_dist < -max_heap[0][0]:
-                heapq.heappop(max_heap)
-                heapq.heappush(max_heap, (-curr_dist, (point[0], point[1])))
+        heap = []
+        for p in points:
+            heapq.heappush(heap, (-(math.pow(p[0], 2) + math.pow(p[1], 2)), p))
 
-        return [[x, y] for _, (x, y) in max_heap]
+            if (len(heap) > k):
+                pp = heapq.heappop(heap)
+
+        ans = []
+        while len(heap) > 0:
+            ans.append(heapq.heappop(heap)[1])
+
+        return ans
