269fd8d1b08d1d154c59a5e29e6cf3f814bf3cb5
shubham88fru <shubham88fru@yahoo.com>
2025-12-21 20:40:28 -0600

    tl;dr - find median from data stream

--- hard/find_median_from_data_stream.py
+++ hard/find_median_from_data_stream.py
@@ -1,32 +1,46 @@
 import heapq
 
+""""""""""""""""""""""""""""
+------------------
+OPTIMAL: Two heaps
+------------------
+TC: O(log(n))
+SC: O(1)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+------------------------------------
+BRUTE: Sort everytime a num is added
+------------------------------------
+TC: O(nlog(n))
+SC: O(1)
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/find-median-in-a-data-stream
 class MedianFinder:
 
     def __init__(self):
-        self.max_heap = []
-        self.min_heap = []
+        self.mih = []
+        self.mxh = []
 
-        heapq.heapify(self.max_heap)
-        heapq.heapify(self.min_heap)
-
+    # Coded based on mik's approach.
     def addNum(self, num: int) -> None:
-        if (len(self.max_heap) == 0 and len(self.min_heap) == 0):
-            heapq.heappush(self.max_heap, (-num, num))
+        if len(self.mxh) == 0 or num <= -self.mxh[0]:
+            heapq.heappush(self.mxh, -num)
         else:
-            if num <= self.max_heap[0][1]:
-                heapq.heappush(self.max_heap, (-num, num))
-            else:
-                heapq.heappush(self.min_heap, num)
+            heapq.heappush(self.mih, num)
 
-            if len(self.max_heap) > len(self.min_heap) + 1:
-                heapq.heappush(self.min_heap, heapq.heappop(self.max_heap)[1])
-            elif len(self.min_heap) > len(self.max_heap):
-                n = heapq.heappop(self.min_heap)
-                heapq.heappush(self.max_heap, (-n, n))
+        if len(self.mih) > len(self.mxh):
+            heapq.heappush(self.mxh, -heapq.heappop(self.mih))
+        elif len(self.mxh) - len(self.mih) > 1:
+            heapq.heappush(self.mih, -heapq.heappop(self.mxh))
 
     def findMedian(self) -> float:
-        sz = len(self.min_heap) + len(self.max_heap)
-        if sz % 2 == 0:
-            return (self.min_heap[0] + self.max_heap[0][1]) / 2
-        return self.max_heap[0][1]
+        if (len(self.mih) + len(self.mxh)) % 2 == 0:
+            return (self.mih[0] - self.mxh[0]) / 2
+
+        return -self.mxh[0]
