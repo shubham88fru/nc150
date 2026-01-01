73d2d641f787e937089a429977d8cc2cd1f824cc
shubham88fru <shubham88fru@yahoo.com>
2025-12-31 19:40:05 -0600

    tl;dr - word search

--- medium/word_search.py
+++ medium/word_search.py
@@ -1,37 +1,54 @@
 from typing import List
 
+""""""""""""""""""""""""""""
+------------
+OPTIMAL: DFS
+------------
+TC: O(m*n*4ˆL); where L is the length of the word to be searched.
+SC: O(L); recursive stack space will be at most length of the word.
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+----------
+BRUTE: 
+----------
+TC:
+SC:
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/search-for-word
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        return self.solve(board, word)
+        return self.revise(board, word)
 
-    def solve(self, board, word):
+    def revise(self, board, word):
         m = len(board)
         n = len(board[0])
 
         for i in range(0, m):
             for j in range(0, n):
-                visited = [[0 for _ in range(n)] for _ in range(m)]
-                if board[i][j] == word[0]:
-                    if self.dfs(board, word, 0, i, j, m, n, visited):
-                        return True
+                if board[i][j] == word[0] and self.dfs(board, word, i, j, 0, m, n):
+                    return True
 
         return False
 
-    def dfs(self, board, word, wi, i, j, m, n, visited):
-        if wi >= len(word):
+    def dfs(self, board, word, i, j, k, m, n):
+        if k >= len(word):
             return True
 
-        if i < 0 or i >= m or j < 0 or j >= n or visited[i][j] != 0 or board[i][j] != word[wi]:
+        if i < 0 or i >= m or j < 0 or j >= n or word[k] != board[i][j] or board[i][j] == '#':
             return False
 
-        visited[i][j] = -1
+        og = board[i][j]
+        board[i][j] = '#'
+        up = self.dfs(board, word, i - 1, j, k + 1, m, n)
+        right = self.dfs(board, word, i, j + 1, k + 1, m, n)
+        down = self.dfs(board, word, i + 1, j, k + 1, m, n)
+        left = self.dfs(board, word, i, j - 1, k + 1, m, n)
+        board[i][j] = og
 
-        up = self.dfs(board, word, wi + 1, i - 1, j, m, n, visited)
-        right = self.dfs(board, word, wi + 1, i, j + 1, m, n, visited)
-        down = self.dfs(board, word, wi + 1, i + 1, j, m, n, visited)
-        left = self.dfs(board, word, wi + 1, i, j - 1, m, n, visited)
-
-        visited[i][j] = 0
-
-        return up or left or right or down
+        return up or right or down or left
