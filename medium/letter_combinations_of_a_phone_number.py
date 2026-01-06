fc18fbc86d9547e04aaa0c2f72df3112a0733f0f
shubham88fru <shubham88fru@yahoo.com>
2026-01-06 16:59:45 -0600

    tl;dr - letter combination of a phone number

--- medium/letter_combinations_of_a_phone_number.py
+++ medium/letter_combinations_of_a_phone_number.py
@@ -1,35 +1,50 @@
 from typing import List
 
+""""""""""""""""""""""""""""
+---------------------
+OPTIMAL: Backtracking
+---------------------
+TC: O(n*4ˆn)
+SC: O(n)
+
+------------------------------------
+BETTER:
+------------------------------------
+TC:
+SC:
+
+----------------------------------------------
+BRUTE:
+----------------------------------------------
+TC:
+SC:
+
+"""""""""""""""""""""""""""
 # @link - https://neetcode.io/problems/combinations-of-a-phone-number
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        return self.solve(digits)
+        return self.revise(digits)
 
-    def solve(self, digits):
-        mp = {}
-        mp['2'] = ['a', 'b', 'c']
-        mp['3'] = ['d', 'e', 'f']
-        mp['4'] = ['g', 'h', 'i']
-        mp['5'] = ['j', 'k', 'l']
-        mp['6'] = ['m', 'n', 'o']
-        mp['7'] = ['p', 'q', 'r', 's']
-        mp['8'] = ['t', 'u', 'v']
-        mp['9'] = ['w', 'x', 'y', 'z']
+    def revise(self, digits):
+        mp = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
 
         ans = []
-        if len(digits) == 0:
-            return ans
-
-        self.backtrack(digits, 0, ans, "", mp)
-
+        self.backtrack(digits, 0, ans, '', mp)
         return ans
 
-    def backtrack(self, digits, wi, ans, curr, mp):
-        if wi >= len(digits):
-            ans.append(curr)
+    def backtrack(self, digits, i, ans, sub, mp):
+        if i >= len(digits):
+            ans.append(sub)
             return
 
-        lst = mp[digits[wi]]
-
-        for i in range(0, len(lst)):
-            self.backtrack(digits, wi+1, ans, curr+lst[i], mp)
+        for el in mp[digits[i]]:
+            self.backtrack(digits, i + 1, ans, sub + el, mp)
